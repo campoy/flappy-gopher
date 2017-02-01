169b05967a3684454561dca273008146cbe05b6a
Francesc Campoy <campoy@golang.com>
2017-02-01 21:38:09 +0100

    paints Flappy Gopher on screen

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
+	ttf "github.com/veandco/go-sdl2/sdl_ttf"
 )
 
 func main() {
@@ -22,15 +23,53 @@ func run() error {
 	}
 	defer sdl.Quit()
 
+	if err := ttf.Init(); err != nil {
+		return fmt.Errorf("could not initialize TTF: %v", err)
+	}
+	defer ttf.Quit()
+
 	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
 	if err != nil {
 		return fmt.Errorf("could not create window: %v", err)
 	}
 	defer w.Destroy()
 
-	_ = r
+	if err := drawTitle(r); err != nil {
+		return fmt.Errorf("could not draw title: %v", err)
+	}
 
 	time.Sleep(5 * time.Second)
+
+	return nil
+}
+
+func drawTitle(r *sdl.Renderer) error {
+	r.Clear()
+
+	f, err := ttf.OpenFont("res/fonts/Flappy.ttf", 20)
+	if err != nil {
+		return fmt.Errorf("could not load font: %v", err)
+	}
+	defer f.Close()
+
+	c := sdl.Color{R: 255, G: 100, B: 0, A: 255}
+	s, err := f.RenderUTF8_Solid("Flappy Gopher", c)
+	if err != nil {
+		return fmt.Errorf("could not render title: %v", err)
+	}
+	defer s.Free()
+
+	t, err := r.CreateTextureFromSurface(s)
+	if err != nil {
+		return fmt.Errorf("could not create texture: %v", err)
+	}
+	defer t.Destroy()
+
+	if err := r.Copy(t, nil, nil); err != nil {
+		return fmt.Errorf("could not copy texture: %v", err)
+	}
+
+	r.Present()
 
 	return nil
 }
