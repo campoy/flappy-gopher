d6ef7718f66e80e47381312b5202e2af5102f15b
Francesc Campoy <campoy@golang.com>
2017-02-01 20:36:57 +0100

    opening a window for 5 seconds

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(2)
+	}
+}
+
+func run() error {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	w, r, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("could not create window: %v", err)
+	}
+	defer w.Destroy()
+
+	_ = r
+
+	time.Sleep(5 * time.Second)
+
+	return nil
+}
